e6d6cdd0c82e884fb1c00dc895f9ede31ebbcaab
NguyenCatNguyen <catngn11@gmail.com>
2024-06-03 14:32:01 -0500

    update

--- book-library/src/components/Featured.jsx
+++ book-library/src/components/Featured.jsx
@@ -1,31 +1,23 @@
 import React from 'react';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {books} from "../data"
+import Book from './ui/Book';
 
 const Featured = () => {
     return(
         <section id='features'>
+            
             <div className="container">
                 <div className="row">
                     <h2 className="section__title">
                         Featured <span className='purple'>Books</span>
                     </h2>
                     <div className="books">
-                        <div className="book">
-                            <a href="">
-                                <figure className="book__img--wrapper">
-                                    <img src="" alt="" className="book__img" />
-                                </figure>
-                            </a>
-                            <div className="boook__title">
-                                <a href="" className="book__title--link"></a>
-                            </div>
-                            <div className="book_ratings">
-
-                            </div>
-                            <div className="book__price">
-                                <span className="book__price--normal">23</span>323
-                            </div>
-                        </div>
+                        {books
+                            .filter((book) => book.rating === 5)
+                            .slice(0,4)
+                            .map((book)=>
+                            (<Book book={book}/>))
+                        }
                     </div>
                 </div>
             </div>
--- book-library/src/components/ui/Book.jsx
+++ book-library/src/components/ui/Book.jsx
@@ -1,1 +1,36 @@
-rfc 
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+
+const Book = ({book}) =>{
+    return(
+        <div className="book">
+                            <a href="">
+                                <figure className="book__img--wrapper">
+                                    <img src={book.url} alt="" className="book__img" />
+                                </figure>
+                            </a>
+                            <div className="boook__title">
+                                <a href="" className="book__title--link">{book.title}</a>
+                            </div>
+                            <div className="book_ratings">
+
+                            </div>
+                            <div className="book__price">
+                                {book.salePrice ? (
+                                    <>
+                                        <span className="book__price--normal">${book.originalPrice.toFi xed(2)}</span>
+                                        ${book.salePrice}
+                                    </>
+                                ) : (
+                                    <>
+                                    ${book.originalPrice}
+                                    </>)
+                            }
+                            </div>
+                        </div>
+
+    );
+};
+
+export default Book
