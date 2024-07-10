dc0ac8aa19d930a487d73a01c4e8b754c6e436c0
NguyenCatNguyen <catngn11@gmail.com>
2024-07-10 09:19:21 -0500

    update

--- elibrary/src/App.js
+++ elibrary/src/App.js
@@ -1,22 +1,19 @@
 
 import Nav from './components/Nav';
-import Landing from './components/Landing';
-import Highlights from './components/Highlights';
-import Featured from './components/Featured';
-import Discounted from './components/Discounted';
-import Explore from './components/Explore';
 import Footer from './components/Footer';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './pages/Home';
 function App() {
   return (
+    <Router>
     <div className="App">
+
       <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
+      <Route path="/" exact component={Home} /> // exact is used to match the exact path
+      <Home />
       <Footer />
     </div>
+    </Router>
   );
 }
 
---
+++ elibrary/src/pages/Books.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Books() {
+  return (
+    <div className="books__body">
+        <main id="books__main">
+            <section>
+                <div className="books__container">
+                    <div className="row">
+                        <div className="books__header">
+                            
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+    </div>
+  )
+}
